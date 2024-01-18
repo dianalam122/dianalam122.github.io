d304dc01a8f1bb4846fdd90ba102fea12f5f757f
dianalam122 <dianalam122@gmail.com>
2024-01-18 03:10:24 -0500

    Remove image

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,7 +3,8 @@ import profileImage from '../assets/profileImage.jpeg';
 
 function Experience() {
     const carouselStyle = {
-        height: '400px', // Set the desired height
+        height: '400px',
+        paddingTop: '60px',
     };
 
     const imageStyle = {
@@ -15,35 +16,20 @@ function Experience() {
 
     return (
         <div className="experienceContainer">
-            <Carousel style={carouselStyle}>
-                <Carousel.Item>
-                    <img
-                        src={profileImage}
-                        alt="First slide"
-                        style={imageStyle}
-                    />
+            <Carousel style={carouselStyle} variant="dark">
+                <Carousel.Item className="carousel-item">
                     <Carousel.Caption>
                         <h3>Job1</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        src={profileImage}
-                        alt="First slide"
-                        style={imageStyle}
-                    />
+                <Carousel.Item className="carousel-item">
                     <Carousel.Caption>
                         <h3>Job2</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        src={profileImage}
-                        alt="First slide"
-                        style={imageStyle}
-                    />
+                <Carousel.Item className="carousel-item">
                     <Carousel.Caption>
                         <h3>Job3</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
